63795fc5364e51acf6a56312dd59cea399f39646
Pat Dill <jamespatrickdill@gmail.com>
2025-11-19 19:42:23 -0500

    placeholder color

--- web/src/features/Settings/Section.tsx
+++ web/src/features/Settings/Section.tsx
@@ -5,35 +5,41 @@ import { Card, Flex, Form } from "antd";
 import { ReactNode, useState } from "react";
 
 type SectionProps = {
-    title: string;
-    description: string;
-    icon: ReactNode;
-    children?: ReactNode;
-    initialValues?: Record<string, any>;
-}
+  title: string;
+  description: string;
+  icon: ReactNode;
+  children?: ReactNode;
+  initialValues?: Record<string, any>;
+};
 
 export function Section(props: SectionProps) {
-    const { title, icon, children } = props;
+  const { title, icon, children } = props;
 
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-    const { data: settings } = useSettings();
+  const { data: settings } = useSettings();
 
-    return (
-        <Card
-            title={<Flex align="center" gap={4}>{icon}<span>{title}</span></Flex>}
-        >
-            <Form
-                form={form}
-                initialValues={{
-                    ...settings,
-                    ...props.initialValues,
-                }}
-            >
-                {children}
-            </Form>
-        </Card>
-    )
+  return (
+    <Card
+      title={
+        <Flex align="center" gap={4}>
+          {icon}
+          <span>{title}</span>
+        </Flex>
+      }
+    >
+      <Form
+        layout="vertical"
+        form={form}
+        initialValues={{
+          ...settings,
+          ...props.initialValues,
+        }}
+      >
+        {children}
+      </Form>
+    </Card>
+  );
 }
--- web/src/features/Settings/SoundSettings.tsx
+++ web/src/features/Settings/SoundSettings.tsx
@@ -2,42 +2,77 @@
 
 import { AudioOutlined } from "@ant-design/icons";
 import { Section } from "./Section";
-import { Form, InputNumber, Select } from "antd";
+import { Flex, Form, InputNumber, Select } from "antd";
 
 import { useDevices } from "@/api/settings/getDevices";
 import { useMemo } from "react";
 
 export function SoundSettingsForm() {
-    const { data: devices } = useDevices();
+  const { data: devices } = useDevices();
 
-    const deviceOptions = useMemo(() => {
-        return devices?.filter(device => device.max_input_channels > 0)?.map((device) => ({
-            label: device.name,
-            value: device.name,
-        })) ?? [];
-    }, [devices]);
+  const deviceOptions = useMemo(() => {
+    return (
+      devices
+        ?.filter((device) => device.max_input_channels > 0)
+        ?.map((device) => ({
+          label: device.name,
+          value: device.name,
+        })) ?? []
+    );
+  }, [devices]);
 
+  const selectedDeviceName = Form.useWatch("device");
+  const selectedDevice = useMemo(() => {
+    return devices?.find((device) => device.name === selectedDeviceName);
+  }, [devices, selectedDeviceName]);
 
-    const selectedDeviceName = Form.useWatch("device");
-    const selectedDevice = useMemo(() => {
-        return devices?.find(device => device.name === selectedDeviceName);
-    }, [devices, selectedDeviceName]);
+  return (
+    <>
+      <Form.Item
+        label="Device"
+        name="device"
+        rules={[{ required: true, message: "Please select a device" }]}
+      >
+        <Select options={deviceOptions} />
+      </Form.Item>
 
-    return <>
-        <Form.Item label="Device" name="device" rules={[{ required: true, message: "Please select a device" }]}>
-            <Select options={deviceOptions} />
+      <Flex gap={12} justify="space-between">
+        <Form.Item label="Channels" name="channels" style={{ flexGrow: 1 }}>
+          <InputNumber
+            placeholder={selectedDevice?.max_input_channels?.toString()}
+            min={1}
+            max={selectedDevice?.max_input_channels}
+            style={{ width: "100%" }}
+          />
         </Form.Item>
 
-        <Form.Item label="Channels" name="channels">
-            <InputNumber placeholder={selectedDevice?.max_input_channels?.toString()} min={1} max={selectedDevice?.max_input_channels} />
+        <Form.Item
+          label="Sample rate"
+          name="sample_rate"
+          style={{ flexGrow: 1 }}
+        >
+          <InputNumber
+            placeholder={selectedDevice?.default_samplerate?.toString()}
+            style={{ width: "100%" }}
+          />
         </Form.Item>
 
-        <Form.Item label="Sample rate" name="sample_rate">
-            <InputNumber placeholder={selectedDevice?.default_samplerate?.toString()} />
+        <Form.Item
+          label="Silence threshold (RMS)"
+          name="silence_threshold"
+          rules={[
+            { required: true, message: "Please enter a silence threshold" },
+          ]}
+          style={{ flexGrow: 1 }}
+        >
+          <InputNumber
+            min={0}
+            max={1}
+            step={0.0001}
+            style={{ width: "100%" }}
+          />
         </Form.Item>
-
-        <Form.Item label="Silence threshold (RMS)" name="silence_threshold" rules={[{ required: true, message: "Please enter a silence threshold" }]}>
-            <InputNumber min={0} max={1} step={0.0001} />
-        </Form.Item>
+      </Flex>
     </>
+  );
 }
