297e1986fdd1c5c09ae15234441e7fe04d975c72
Pat Dill <jamespatrickdill@gmail.com>
2025-11-24 13:44:47 -0500

    Update useWebsocketHost.ts

--- web/src/api/useWebsocketHost.ts
+++ web/src/api/useWebsocketHost.ts
@@ -2,15 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { get } from "./request";
 
 export function getWebsocketHost(): Promise<string> {
-  return get(
-    `/api/status/websocket-host?scheme=${window.location.protocol.replace(":", "")}`,
-  ).then((res) => res.json());
+    return get(
+        `/api/status/websocket-host?scheme=${window.location.protocol.replace(":", "")}`,
+    ).then((res) => res.json());
 }
 
 export function useWebsocketHost() {
-  return useQuery({
-    queryKey: ["websocket-host"],
-    queryFn: () => getWebsocketHost(),
-    staleTime: 0,
-  });
+    return useQuery({
+        queryKey: ["websocket-host"],
+        queryFn: () => getWebsocketHost(),
+        staleTime: 0,
+    });
 }
