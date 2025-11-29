448bcecff90bd1a3e78eeb7f4c6fbb9318be79ea
Pat Dill <jamespatrickdill@gmail.com>
2025-11-29 12:59:55 -0500

    add form

--- web/src/features/History/History.tsx
+++ web/src/features/History/History.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQueryClient } from "@tanstack/react-query";
-import { CSSProperties, Fragment, useEffect } from "react";
+import { CSSProperties, Fragment, useEffect, useState } from "react";
 import HistoryEntry from "@/features/History/HistoryEntry";
 import { HistoryEntryT } from "@/schemas";
 import { formatDurationLong } from "@/utils/formatDurationShort";
@@ -12,6 +12,8 @@ import { Link } from "next-view-transitions";
 import { Flex, Typography } from "antd";
 import useAutoLoadMore from "@/utils/useAutoLoadMore";
 import useSafeClientSplit from "@/utils/useSafeClientSplit";
+import { PlusOutlined } from "@ant-design/icons";
+import { ManualEntryModal } from "./ManualEntryModal";
 
 export default function History({ style }: { style?: CSSProperties }) {
   const queryClient = useQueryClient();
@@ -32,65 +34,82 @@ export default function History({ style }: { style?: CSSProperties }) {
 
   const entries = history.data?.pages?.map((page) => page.data).flat() || [];
 
+  const [manualEntryOpen, setManualEntryOpen] = useState(false);
+
   return (
-    <Flex
-      vertical
-      style={{
-        ...style,
-      }}
-    >
-      <Link href="/history" style={{ alignSelf: "start" }}>
+    <>
+      <Flex
+        vertical
+        style={{
+          ...style,
+        }}
+      >
         <Typography.Title
           level={4}
           style={{ opacity: 0.9, viewTransitionName: "history-title" }}
         >
-          History
+          <Flex align="center" justify="space-between">
+            <span>History</span>
+
+            <button
+              onClick={() => setManualEntryOpen(true)}
+              style={{ fontSize: 16 }}
+            >
+              <PlusOutlined />
+            </button>
+          </Flex>
         </Typography.Title>
-      </Link>
 
-      <Flex vertical gap={6}>
-        {entries.map((entry: HistoryEntryT, idx, entries) => {
-          const prevEntry = entries[idx - 1];
-          if (entry.entry_id === prevEntry?.entry_id) return;
+        <Flex vertical gap={6}>
+          {entries.map((entry: HistoryEntryT, idx, entries) => {
+            const prevEntry = entries[idx - 1];
+            if (entry.entry_id === prevEntry?.entry_id) return;
 
-          let timeMarker: string | null = null;
-          if (idx > 0) {
-            const secondsSinceDetected =
-              (new Date().valueOf() - dayjs(entry.detected_at).valueOf()) /
-              1000;
-            const prevSecondsSinceDetected =
-              (new Date().valueOf() - dayjs(prevEntry.detected_at).valueOf()) /
-              1000;
+            let timeMarker: string | null = null;
+            if (idx > 0) {
+              const secondsSinceDetected =
+                (new Date().valueOf() - dayjs(entry.detected_at).valueOf()) /
+                1000;
+              const prevSecondsSinceDetected =
+                (new Date().valueOf() -
+                  dayjs(prevEntry.detected_at).valueOf()) /
+                1000;
 
-            const roundTo = 3600;
-            const sinceDetected = formatDurationLong(
-              Math.floor(secondsSinceDetected / roundTo) * roundTo,
-            );
-            const prevSinceDetected = formatDurationLong(
-              Math.floor(prevSecondsSinceDetected / roundTo) * roundTo,
-            );
+              const roundTo = 3600;
+              const sinceDetected = formatDurationLong(
+                Math.floor(secondsSinceDetected / roundTo) * roundTo,
+              );
+              const prevSinceDetected = formatDurationLong(
+                Math.floor(prevSecondsSinceDetected / roundTo) * roundTo,
+              );
 
-            if (sinceDetected !== prevSinceDetected) {
-              timeMarker = sinceDetected;
+              if (sinceDetected !== prevSinceDetected) {
+                timeMarker = sinceDetected;
+              }
             }
-          }
 
-          return (
-            <Fragment key={entry.entry_id}>
-              {timeMarker && (
-                <div className="flex items-center justify-center gap-2 select-none">
-                  <div className="flex-grow h-[1px] bg-current opacity-40" />
-                  <Typography.Text style={{ opacity: 0.75 }}>
-                    {timeMarker} ago
-                  </Typography.Text>
-                  <div className="flex-grow h-[1px] bg-current opacity-40" />
-                </div>
-              )}
-              <HistoryEntry entry={entry} />
-            </Fragment>
-          );
-        })}
+            return (
+              <Fragment key={entry.entry_id}>
+                {timeMarker && (
+                  <div className="flex items-center justify-center gap-2 select-none">
+                    <div className="flex-grow h-[1px] bg-current opacity-40" />
+                    <Typography.Text style={{ opacity: 0.75 }}>
+                      {timeMarker} ago
+                    </Typography.Text>
+                    <div className="flex-grow h-[1px] bg-current opacity-40" />
+                  </div>
+                )}
+                <HistoryEntry entry={entry} />
+              </Fragment>
+            );
+          })}
+        </Flex>
       </Flex>
-    </Flex>
+
+      <ManualEntryModal
+        open={manualEntryOpen}
+        onClose={() => setManualEntryOpen(false)}
+      />
+    </>
   );
 }
