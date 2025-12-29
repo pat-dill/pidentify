9e35c02e7820a6ee82cff0c5dca45bc68aa4c07d
Pat Dill <jamespatrickdill@gmail.com>
2025-12-28 23:51:47 -0500

    Update AuthSettings.tsx

--- web/src/features/Settings/AuthSettings.tsx
+++ web/src/features/Settings/AuthSettings.tsx
@@ -8,21 +8,32 @@ export function AuthSettingsForm() {
 
   return (
     <>
-      <Form.Item label="Username" name="admin_username" rules={[{ required: true, message: "Please enter your username" }]}>
-        <Input
-          placeholder="Username"
-        />
+      <Form.Item
+        label="Username"
+        name="admin_username"
+        rules={[{ required: true, message: "Please enter your username" }]}
+      >
+        <Input placeholder="Username" />
       </Form.Item>
 
       <Flex gap={12} justify="space-between">
-        <Form.Item label="Current Password" name="old_password" style={{ width: "50%" }} hidden={!settings?.has_password}>
+        <Form.Item
+          label="Current Password"
+          name="old_password"
+          style={{ width: "50%" }}
+          hidden={!settings?.has_password}
+        >
           <Input.Password
             placeholder="Current Password"
             style={{ width: "100%" }}
           />
         </Form.Item>
 
-        <Form.Item label="New Password" name="new_password" style={{ width: "50%" }}>
+        <Form.Item
+          label="New Password"
+          name="new_password"
+          style={{ width: settings?.has_password ? "50%" : "100%" }}
+        >
           <Input.Password
             placeholder="New Password"
             style={{ width: "100%" }}
